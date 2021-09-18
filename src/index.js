597d1dc3e28a08d2be406ed713614d4590d2bada
Oleg <Oleg17.k@gmail.com>
2021-09-18 11:04:05 +0300

    directories and App.js created

--- src/index.js
+++ src/index.js
@@ -1,40 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Text = () => {
-    return (
-        <>
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Pariatur ab assumenda earum explicabo. Earum beatae magni aut
-                aspernatur assumenda asperiores iure rerum porro unde molestiae
-                quis, in corrupti pariatur error?
-            </p>
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Pariatur ab assumenda earum explicabo. Earum beatae magni aut
-                aspernatur assumenda asperiores iure rerum porro unde molestiae
-                quis, in corrupti pariatur error?
-            </p>
-        </>
-    )
-}
-
-const Title = (props) => {
-    console.log(props)
-    return <h1>Hello {props.name}</h1>
-}
-
-const App = () => {
-    return (
-        <>
-            <Title name="Jack" />
-            <Text />
-            <Title name="Oleg" />
-            <Title name="Test" />
-            <Title name="React.js" />
-        </>
-    )
-}
+import App from './container/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
