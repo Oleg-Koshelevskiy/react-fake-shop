3b8e81e1c6547854e872101035c407327b73db5e
Oleg <Oleg17.k@gmail.com>
2021-09-19 13:35:10 +0300

    6 items added

--- src/components/Products/ProductListItem.js
+++ src/components/Products/ProductListItem.js
@@ -3,15 +3,15 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import { Button, CardActions } from '@mui/material'
 
-const ProductListItem = (props) => {
+const ProductListItem = ({ name, description, type, capacity, price }) => {
     return (
         <Card>
             <CardContent>
-                <h4>{props.name}</h4>
-                <p>{props.description}</p>
-                <div>{props.type}</div>
-                <div>{props.capacity}Gb</div>
-                <div>{props.price}$</div>
+                <h4>{name}</h4>
+                <p>{description}</p>
+                <div>{type}</div>
+                <div>{capacity}Gb</div>
+                <div>{price}$</div>
             </CardContent>
             <CardActions>
                 <Button>Add to Cart</Button>
---
+++ src/components/Products/productsArray.js
@@ -0,0 +1,46 @@
+const productsArray = [
+    {
+        name: 'iPhone XS',
+        description: 'This is iPhone XS',
+        type: 'phone',
+        capacity: '64',
+        price: '500',
+    },
+    {
+        name: 'iPhone X',
+        description: 'This is iPhone X',
+        type: 'phone',
+        capacity: '128',
+        price: '1000',
+    },
+    {
+        name: 'iPhone 12',
+        description: 'This is iPhone 12',
+        type: 'phone',
+        capacity: '256',
+        price: '1000',
+    },
+    {
+        name: 'iPhone 8',
+        description: 'This is iPhone 8',
+        type: 'phone',
+        capacity: '32',
+        price: '250',
+    },
+    {
+        name: 'iPhone 13',
+        description: 'This is iPhone 13',
+        type: 'phone',
+        capacity: '512',
+        price: '1500',
+    },
+    {
+        name: 'iPhone 11',
+        description: 'This is iPhone 11',
+        type: 'phone',
+        capacity: '256',
+        price: '800',
+    },
+]
+
+export default productsArray
