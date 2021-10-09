fdf680a2fc323842f405b5944ff18631abe4453b
Oleg <oleg17.k@gmail.com>
2021-10-09 11:48:46 +0300

    get cart items from object

--- src/components/Cart/CartHeader.js
+++ src/components/Cart/CartHeader.js
@@ -2,13 +2,23 @@ import React from 'react'
 import { keys } from 'lodash'
 import productsArray from '../Products/productsArray'
 
+const productObject = productsArray.reduce(
+    (object, product) => ({
+        ...object,
+        [product.id]: product,
+    }),
+    {}
+)
+
+console.log(productObject)
+console.log(productsArray)
+
 const CartHeader = ({ productsInCart }) => {
     return (
         <div>
             {keys(productsInCart).map((productId) => (
                 <div key={productId}>
-                    {productsArray[productId - 1].name} :
-                    {productsInCart[productId]}
+                    {productObject[productId].name} :{productsInCart[productId]}
                 </div>
             ))}
         </div>
